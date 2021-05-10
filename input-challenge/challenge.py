4efc0b74028fe94a638c1f02bcd569ec03297011
PAT WILLIAMS <plwg123@outlook.com>
2021-05-10 19:33:33 +0000

    challenge for inputs

---
+++ input-challenge/challenge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_name = input("please enter name: ")
+day_of_week = input("please enter the day of week for today: ")
+print("hello ",user_name,"! ","happy ",day_of_week,"!",sep="")
+
+

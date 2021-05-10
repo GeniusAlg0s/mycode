05ec890b686fc0dfa62e6f886aa92c2521e9ef94
PAT WILLIAMS <plwg123@outlook.com>
2021-05-10 21:19:20 +0000

    second challege of the day

---
+++ challenge2/li_chal_two.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+user_name=input("please enter your name : ")
+
+icecream =["flavors", "salty"]
+print(icecream)
+icecream.append(99)
+print(icecream)
+print(icecream[2], "", icecream[0], "and ",user_name, "chooses to be ", icecream[1])

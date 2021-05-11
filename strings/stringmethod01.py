d34d9a6ff0d62c6646f43272862b1a267b22c18d
PAT WILLIAMS <plwg123@outlook.com>
2021-05-11 13:10:40 +0000

    pat williams join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+lilstring ="Alta3 Research offers classes on Python coding"
+newlist= lilstring.split(" ") #new list here
+print(newlist)
+
+#list of stirngs
+myiplist=["192","168","0","12"]
+
+# set singleip as the result of joining the list myiplist around the "."
+singleip =".".join(myiplist)
+
+# display singleip
+print(singleip)
+

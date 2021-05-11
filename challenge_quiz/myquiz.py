90819f48866f5157545d798a7138f2775f35df70
PAT WILLIAMS <plwg123@outlook.com>
2021-05-11 22:11:24 +0000

    added a list for responses aka print msgs

--- challenge_quiz/myquiz.py
+++ challenge_quiz/myquiz.py
@@ -19,7 +19,7 @@ while True:
        age= int(age)
        break
 
-
+response = ["we guess you may not like too many people","you be bored with out people","try again","please dont be dumbe enter the right stuff","you should live in a farming community","middle of no where is good for you","do you cummute by bus or car?","you should live in a inner city","suburbs should be fine"]
 #highest level
 people_pref =""
 
@@ -32,16 +32,16 @@ travel_pref=""
 print("you are ", age)
 
 if age >=50:
-    print("we guess you may not like too many people")
+    print(response[0])
 
 if age <50:
-    print("you be bored with out people")
+    print(response[1])
 
 while people_pref == "":
     people_pref = input("do you perfer loads of people or few? ")
     if people_pref != "few":
         if people_pref != "loads":
-         print("try again")
+         print(response[2])
          people_pref = ""
 
 
@@ -50,23 +50,23 @@ if people_pref == "few":
         corn_pref = input("do you perfer farm land or woodland? ")
         if corn_pref != "farm":
             if corn_pref != "woodland":
-                print("please dont be dumbe enter the right stuff")
+                print(response[3])
                 corn_pref =""
         if corn_pref == "farm":
-                print("you should live in a farming community")
+                print(response[4])
         if corn_pref == "woodland":
-                print("middle of no where is good for you") 
+                print(response[5]) 
 
 if people_pref == "loads":
     while travel_pref == "":
-        travel_pref = input("do you cummute by bus or car?")
+        travel_pref = input(response[6])
         if travel_pref != "bus":
             if travel_pref != "car":
-                print("please dont be dumbe enter the right stuff")
+                print(response[3])
                 corn_pref =""
         if travel_pref == "bus":
-            print("you should live in a inner city")
+            print(response[7])
             break
         if travel_pref == "car":
-            print("suburbs should be fine")
+            print(response[8])
             break

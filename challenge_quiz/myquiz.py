2f5f179ee818de20bd2cc211f03a8405a529ce42
PAT WILLIAMS <plwg123@outlook.com>
2021-05-11 19:02:13 +0000

    start of my challenge

---
+++ challenge_quiz/myquiz.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#highest level
+people_pref =""
+
+#few path
+
+
+#loads pth
+
+
+
+
+
+while people_pref == "":
+    people_pref = input("do you perfer loads of people or few?")
+    if people_pref != "few":
+        if people_pref != "loads":
+         print("try again")
+         people_pref = ""
+#if people_pref == "few";
+
+

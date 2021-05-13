59459ec88f8a8f15789ea686f8c7b6bc587086cd
PAT WILLIAMS <plwg123@outlook.com>
2021-05-13 10:27:13 +0000

    calculator is finished cant divide by 0 and the three operations works

--- funcchal/cal.py
+++ funcchal/cal.py
@@ -25,9 +25,19 @@ def valop(op):
         return True
     else:
         return False
+def mathtime(n1,n2,op):
+    operators=["x","/","-","+"]
+    if op == operators[0]:
+        print(n1*n2)
+    elif op == operators[1]:
+        print(n1/n2)
+    elif op == operators[2]:
+        print(n1-n2)
+    else:
+        print(n1+n2)
 
 
-operators=["x","/","-","+"]
+
 num1=""
 num2=""
 uoperator=""
@@ -56,13 +66,17 @@ while True:
         num2= float(num2)
         break
 
-print("you first number is: ", num1, "your second number is: ", num2)
+print("you first number is: ", num1, "your second number is: ", num2, type(num2))
 
 while True:
     uoperator= input("choose x / - or + for some math time")
     if not valop(uoperator):
         print("stop playing not fun")
+    elif num2 == 0 and uoperator == "/":
+        print("cant divide be zero dummy")
     else:
-        print("your operation to be performed is : ", uoperator)
+        print("your operation to be performed is : ", uoperator, type(uoperator))
         break
+
+mathtime(num1,num2,uoperator)
 

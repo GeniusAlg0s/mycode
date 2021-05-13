290f4d1a54b3872f29436a4af0c5d6f9c924639f
PAT WILLIAMS <plwg123@outlook.com>
2021-05-13 14:52:28 +0000

    start challenge

---
+++ thurschal/triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/evn python3
+
+
+stars= ["*","**","***","****","*****","****","***","**","*"]
+def main():
+    stars= ["*","**","***","****","*****","****","***","**","*"]
+    for x in range(len(stars)):
+        print(stars[x])
+
+main()

58e040984a165faaadb5f655a17a8997b7e55316
PAT WILLIAMS <plwg123@outlook.com>
2021-05-14 17:07:37 +0000

    misc

--- requestPokemon/poke.py
+++ requestPokemon/poke.py
@@ -17,34 +17,38 @@ url2 = "https://pokeapi.co/api/v2/pokemon/"
 respons = requests.get(url)
 respons2 = requests.get(url2)
 
-while True:
-    usrsrch= input("enter number to search for pokemon")
-    if not valint(usrsrch):
-        print("try again only numbers")
-    else:
-        uurl= url2 + usrsrch
-        print(uurl)
-        break
-usrres = requests.get(uurl)
+def main():
+
+    while True:
+        usrsrch= input("enter number to search for pokemon")
+        if not valint(usrsrch):
+            print("try again only numbers")
+        else:
+            uurl= url2 + usrsrch
+            print(uurl)
+            break
+    usrres = requests.get(uurl)
 
 #print(respons.json())
 #print(response.json())
 
-upokedex = usrres.json()
+    upokedex = usrres.json()
 
-print(upokedex)
+    print(upokedex)
 #pokedex = respons.json()
 
-for x in upokedex["moves"]:
-    print(x["move"]["name"])
-print(upokedex["sprites"]["front_default"])
+    for x in upokedex["moves"]:
+        print(x["move"]["name"])
+    print(upokedex["sprites"]["front_default"])
 
-imgfile= upokedex["sprites"]["front_default"]
+    imgfile= upokedex["sprites"]["front_default"]
 
 
-count = 0
-for x in upokedex["game_indices"]:
-     count = count +1
-print(count)
+    count = 0
+    for x in upokedex["game_indices"]:
+         count = count +1
+    print(count)
+    print(upokedex["name"])
 #dfile= wget.download(imgfile)
+main()
 
